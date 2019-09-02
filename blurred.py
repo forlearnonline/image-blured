6e3e6a04b2a6c589e22781ac79d808b4872db8ca
rajatdotcom <54785312+rajatdotcom@users.noreply.github.com>
2019-09-02 08:07:10 +0530

    Little changes in function

--- blurred.py
+++ blurred.py
@@ -7,10 +7,10 @@ import numpy as np
 
 
 def blurred(img):
-    avg_img =(    img[1:-1 ,1:-1]  # center
+    img_slice =(    img[1:-1 ,1:-1]  # center
                 + img[ :-2 ,1:-1]  # top
                 + img[2:   ,1:-1]  # bottom
                 + img[1:-1 , :-2]  # left
                 + img[1:-1 ,2:  ]  # right
                 ) / 5.0
-    return avg_img
+    return img_slice
