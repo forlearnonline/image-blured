e3b2ca9b27802fb4f8357f9b9f99d498df6e389f
rajatdotcom <54785312+rajatdotcom@users.noreply.github.com>
2019-09-02 08:03:55 +0530

    Defining function

--- blurred.py
+++ blurred.py
@@ -4,3 +4,13 @@
 import matplotlib.pyplot as plt
 
 import numpy as np
+
+
+def smooth(img):
+    avg_img =(    img[1:-1 ,1:-1]  # center
+                + img[ :-2 ,1:-1]  # top
+                + img[2:   ,1:-1]  # bottom
+                + img[1:-1 , :-2]  # left
+                + img[1:-1 ,2:  ]  # right
+                ) / 5.0
+    return avg_img
